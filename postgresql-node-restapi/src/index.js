c7ed81d049b37681b9f2904bb3770b76337b88b3
Jvidado <juliandm2004@gmail.com>
2026-05-06 05:33:49 -0500

    Fix: eliminar submodulo y agregar proyecto completo

---
+++ postgresql-node-restapi/src/index.js
@@ -0,0 +1,20 @@
+import cors from "cors";
+import express from "express";
+import usersRoutes from "./routes/users.routes.js";
+import morgan from "morgan";
+import { PORT } from "./config.js";
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(cors());
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(usersRoutes);
+
+app.listen(PORT);
+// eslint-disable-next-line no-console
+console.log("Server on port", PORT);
